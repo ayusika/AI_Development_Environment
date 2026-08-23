15b4a14282e1966bfbe71c9ab14732c79848053f
ayusika <37104099+ayusika@users.noreply.github.com>
2026-08-23 13:33:11 +0900

    KoppyOS: execute proposal f6f156ffab641e94

--- 600_KoppyOS/server/api/v1/sales-master.php
+++ 600_KoppyOS/server/api/v1/sales-master.php
@@ -6,6 +6,10 @@ header(
     'Content-Type: application/json; charset=utf-8'
 );
 
+require_once __DIR__ . '/../../auth/auth.php';
+
+koppyRequireApiAuth();
+
 require_once __DIR__ . '/lib/database.php';
 
 
