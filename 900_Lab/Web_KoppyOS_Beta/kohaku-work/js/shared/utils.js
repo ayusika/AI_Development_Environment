3d4bd0fe8814b30a541b71ddf91d3420b27ad63a
ayusika <37104099+ayusika@users.noreply.github.com>
2026-08-28 16:28:12 +0900

    KoppyOS: execute proposal 4daa310ae1377ee0

---
+++ 900_Lab/Web_KoppyOS_Beta/kohaku-work/js/shared/utils.js
@@ -0,0 +1,11 @@
+function escapeHtml(
+  value
+) {
+
+  return String(value)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
+}
