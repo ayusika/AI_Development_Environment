6c04c70c258f967913df84016fa40ea90e65df8d
ayusika <37104099+ayusika@users.noreply.github.com>
2026-09-03 03:13:42 +0900

    KoppyOS: execute proposal d043287955295839

--- 900_Lab/Web_KoppyOS_Beta/kohaku-work/js/schedule-period.js
+++ 900_Lab/Web_KoppyOS_Beta/kohaku-work/js/schedule-period.js
@@ -43,6 +43,111 @@
   }
 
 
+  function getSchedulePeriod() {
+
+    const {
+      scheduleState,
+      scheduleParseDate,
+      scheduleFormatDate,
+    } = getContext();
+
+
+    const anchor =
+      scheduleParseDate(
+        scheduleState.anchorDate
+        || scheduleFormatDate(
+          new Date()
+        )
+      );
+
+
+    let start =
+      new Date(anchor);
+
+    let days = 1;
+
+
+    if (
+      scheduleState.view === 'week'
+      || scheduleState.view === 'two-weeks'
+    ) {
+
+      const day =
+        start.getDay();
+
+      const mondayOffset =
+        day === 0
+          ? -6
+          : 1 - day;
+
+
+      start.setDate(
+        start.getDate()
+        + mondayOffset
+      );
+
+
+      days =
+        scheduleState.view === 'week'
+          ? 7
+          : 14;
+    }
+
+
+    const end =
+      new Date(start);
+
+
+    end.setDate(
+      end.getDate()
+      + days
+      - 1
+    );
+
+
+    const dates = [];
+
+
+    for (
+      let index = 0;
+      index < days;
+      index += 1
+    ) {
+
+      const date =
+        new Date(start);
+
+
+      date.setDate(
+        start.getDate()
+        + index
+      );
+
+
+      dates.push(
+        scheduleFormatDate(
+          date
+        )
+      );
+    }
+
+
+    return {
+      start:
+        scheduleFormatDate(
+          start
+        ),
+
+      end:
+        scheduleFormatDate(
+          end
+        ),
+
+      dates,
+    };
+  }
+
+
   window.KohakuSchedulePeriod = {
     init,
 
@@ -51,6 +156,7 @@
     },
 
     getContext,
+    getSchedulePeriod,
   };
 
 
