0205fa5a6d5ba6439a418898868109d48a41db36
ayusika <37104099+ayusika@users.noreply.github.com>
2026-09-03 03:06:29 +0900

    KoppyOS: execute proposal 07a16dd3a3e019bc

---
+++ 900_Lab/Web_KoppyOS_Beta/kohaku-work/js/schedule-period.js
@@ -0,0 +1,55 @@
+(() => {
+  'use strict';
+
+
+  let context = null;
+
+
+  function init({
+    scheduleState,
+    scheduleDateInput,
+    schedulePeriodTitle,
+    scheduleParseDate,
+    scheduleFormatDate,
+    loadSchedule,
+  }) {
+
+    if (context) {
+      return;
+    }
+
+
+    context = {
+      scheduleState,
+      scheduleDateInput,
+      schedulePeriodTitle,
+      scheduleParseDate,
+      scheduleFormatDate,
+      loadSchedule,
+    };
+  }
+
+
+  function getContext() {
+
+    if (!context) {
+      throw new Error(
+        'KohakuSchedulePeriod is not initialized.'
+      );
+    }
+
+
+    return context;
+  }
+
+
+  window.KohakuSchedulePeriod = {
+    init,
+
+    isActive() {
+      return Boolean(context);
+    },
+
+    getContext,
+  };
+})();
