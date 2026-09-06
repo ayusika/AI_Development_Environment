a02209cb19c9eee732a2ccfa2dafa331acc20b00
ayusika <37104099+ayusika@users.noreply.github.com>
2026-09-06 17:28:11 +0900

    KoppyOS: execute proposal 6780beee7f9bb240

--- 600_KoppyOS/server/api/v1/lib/visit-sales-calculator.php
+++ 600_KoppyOS/server/api/v1/lib/visit-sales-calculator.php
@@ -1405,6 +1405,7 @@ function koppyConfirmVisitSales(
                     store_course_rate_id = ?,
                     base_price_snapshot = ?,
                     course_take_home_snapshot = ?,
+                    nomination_fee_snapshot = ?,
                     option_price_total_snapshot = ?,
                     option_take_home_total_snapshot = ?,
                     tip_amount = ?,
